d5f947f281079d716cf9b8f8e3da56568fd4bded
KatieCode-6 <katrinkovacova@gmial.com>
2020-06-23 22:10:08 +0100

    Added relevant icon

--- src/app.js
+++ src/app.js
@@ -31,16 +31,27 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  
+  
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+      "src", 
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      );
+    
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+
+
 }
 
 let apiKey = "025dbd8bd1860456ace0cde3c332dc7f";
-let city = "London";
+let city = "Palma de Mallorca";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
