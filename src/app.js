32b2b9111016b0d13f09627730e5538e8726594c
KatieCode-6 <katrinkovacova@gmial.com>
2020-06-24 12:24:38 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -46,12 +46,22 @@ function displayTemperature(response) {
       );
     
     iconElement.setAttribute("alt", response.data.weather[0].description);
+}
 
+function search(city) {
+let apiKey = "025dbd8bd1860456ace0cde3c332dc7f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
 
 }
 
-let apiKey = "025dbd8bd1860456ace0cde3c332dc7f";
-let city = "Palma de Mallorca";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+search("Palma de Mallorca");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
